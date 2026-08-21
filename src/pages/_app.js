32e04ad5bfcb5f94b4126436f9969a71971bcad6
Pramod K <74644178+pramod8095@users.noreply.github.com>
2026-08-21 11:14:13 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,15 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-99RFDCS7HT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-99RFDCS7HT');
+</script>
     </main>
   );
 }
